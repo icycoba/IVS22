4315221c7daab580dc6f2f5d42ff5143f7495a8e
icycoba <icycoba@gmail.com>
2022-03-18 21:55:25 +0100

    Add mathlib structure. Add basic tests.

--- src/mathlib.py
+++ src/mathlib.py
@@ -8,11 +8,78 @@ Popis:      Knihovna pro matematické operace
 
 
 class MathOperations:
-    @staticmethod
-    def add(a, b):
+    """Trida slouzici pro praci s matematickou knihovnou"""
+
+    def __init__(self, num=0):
+        """Inicializace promenne ans, defaultni hodnota je 0"""
+        self.ans = num
+
+    def getvalue(self):
+        """Ziskani hodnoty promenne ans"""
         pass
 
-    @staticmethod
-    def sub(a, b):
+    def add(self, num):
+        """
+        Pricteni hodnoty num k promenne ans
+
+        ans = ans + num
+
+        :param num: hodnota, ktera bude prictena
+        :return: funkce vraci hodnotu ans po secteni s hodnotou num
+        """
         pass
-    # ...
+
+    def sub(self, num):
+        """
+        Odecteni hodnoty num od promenne ans
+
+        ans = ans - num
+
+        :param num: hodnota, ktera bude odectena
+        :return: funkce vraci hodnotu ans po odecteni hodnoty num
+        """
+        pass
+
+    def mul(self, num):
+        """
+        Nasobeni hodnoty num s promennou ans
+
+        ans = ans * num
+
+        :param num: hodnota, kterou se bude nasobit
+        :return: funkce vraci hodnotu ans po vynasobeni hodnotou num
+        """
+        pass
+
+    def div(self, num):
+        """
+        Deleni promenne ans hodnotou num
+
+        ans = ans / num
+
+        :param num: hodnota, kterou se bude delit
+        :return: funkce vraci hodnotu ans po vydeleni hodnotou num
+        """
+        pass
+
+    def pow(self, num):
+        """
+        Umocneni promenne ans hodnotou num
+
+        ans = ans^num
+
+        :param num: hodnota, kterou se bude umocnovat
+        :return: funkce vraci hodnotu ans po umocneni hodnotou num
+        """
+        pass
+
+    def sqrt(self, num):
+        """
+        Odmocneni promenne ans hodnotou num
+
+        ans = ans^(1/num)
+
+        :param num: hodnota, kterou se bude odmocnovat
+        :return: funkce vraci hodnotu ans po odmocneni hodnotou num
+        """
+        pass
--- src/mathlibtests.py
+++ src/mathlibtests.py
@@ -8,8 +8,28 @@ Popis:      Testy pro tvorbu matematické knihovny
 
 
 from mathlib import MathOperations
+import unittest
+
+
+class TestBasicMathOperations(unittest.TestCase):
+    def test_add_1(self):
+        calculator = MathOperations()
+        self.assertEqual(calculator.add(9.21), 9.21)
+        self.assertEqual(calculator.add(0.79), 10)
+        self.assertEqual(calculator.add(.5), 10.5)
+        self.assertEqual(calculator.add(-.5), 10)
+        self.assertEqual(calculator.add(2e2), 210)
+        self.assertEqual(calculator.add(-2e2), 10)
+        self.assertEqual(calculator.add(0), 10)
+        self.assertEqual(calculator.add(-0), 10)
+
+    def test_add_2(self):
+        calculator = MathOperations()
+        self.assertEqual(calculator.add(calculator.add(20)), 40)
+        self.assertEqual(calculator.add(calculator.add(0)), 80)
+        self.assertEqual(calculator.add(calculator.add(-40)), 80)
+        self.assertEqual(calculator.add(calculator.add(-80)), 0)
+
 
 if __name__ == "__main__":
-    MathOperations.add(1, 1)
-    MathOperations.sub(2, 1)
-    # ...
+    unittest.main()
