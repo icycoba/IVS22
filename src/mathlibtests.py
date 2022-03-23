1f2c3f9660916add54492287f367ae5989a205d1
icycoba <icycoba@gmail.com>
2022-03-23 17:51:11 +0100

    Refactor code. Add more basic tests.

--- src/mathlibtests.py
+++ src/mathlibtests.py
@@ -10,19 +10,38 @@ Popis:      Testy pro tvorbu matematické knihovny
 from mathlib import MathOperations
 import unittest
 
+validkeys = [1, -1, 3.5, -6.2, 2e5, -6e7, 2e-3, 0, -0, 1+1, 3-5, 1*5, 52/5]
+validkeysdiv = [1, -1, 3.5, -6.2, 2e5, -6e7, 2e-3, 1+1, 3-5, 1*5, 52/5]
+invalidkeys = ['1', '-1', '0', 'asd', '10asd', True, False, [1, 3, 5], (3, 4, 5), {1, 3, 5}, {"number": 1, "next": 4}]
+invalidkeys2 = [3j, range(6), frozenset({1, 3, 5}), b"byteString", bytearray(5), memoryview(bytes(5))]
+
 
 class TestBasicMathOperations(unittest.TestCase):
+    def test_init_1(self):
+        MathOperations()
+        for key in validkeys:
+            MathOperations(key)
+
+    def test_init_2(self):
+        for key in invalidkeys:
+            MathOperations(key)
+
+    def test_init_3(self):
+        for key in invalidkeys2:
+            MathOperations(key)
+
     def test_add_1(self):
+        results = [0] * len(validkeys)
+        for i, _ in enumerate(validkeys):
+            if i == 0:
+                results[i] += validkeys[i]
+            else:
+                results[i] = results[i-1] + validkeys[i]
+        zippedkeys = zip(validkeys, results)
         calculator = MathOperations()
         self.assertEqual(calculator.getvalue(), 0)
-        self.assertEqual(calculator.add(9.21), 9.21)
-        self.assertEqual(calculator.add(0.79), 10)
-        self.assertEqual(calculator.add(.5), 10.5)
-        self.assertEqual(calculator.add(-.5), 10)
-        self.assertEqual(calculator.add(2e2), 210)
-        self.assertEqual(calculator.add(-2e2), 10)
-        self.assertEqual(calculator.add(0), 10)
-        self.assertEqual(calculator.add(-0), 10)
+        for key, result in zippedkeys:
+            self.assertEqual(calculator.add(key), result)
 
     def test_add_2(self):
         calculator = MathOperations()
@@ -35,63 +54,83 @@ class TestBasicMathOperations(unittest.TestCase):
     def test_add_3(self):
         calculator = MathOperations(20)
         calculator2 = MathOperations(10)
-        calculator3 = MathOperations("String")
         self.assertEqual(calculator.add(calculator2.getvalue()), 30)
         self.assertEqual(calculator2.add(calculator.getvalue()), 40)
 
     def test_sub_1(self):
+        results = [0] * len(validkeys)
+        for i, _ in enumerate(validkeys):
+            if i == 0:
+                results[i] -= validkeys[i]
+            else:
+                results[i] = results[i-1] - validkeys[i]
+        zippedkeys = zip(validkeys, results)
         calculator = MathOperations()
         self.assertEqual(calculator.getvalue(), 0)
-        self.assertEqual(calculator.sub(10), -10)
-        self.assertEqual(calculator.sub(-20), 10)
-        self.assertEqual(calculator.sub(9.78), 0.22)
-        self.assertEqual(calculator.sub(-19.39), 19.61)
-        self.assertEqual(calculator.sub(.5), 19.11)
-        self.assertEqual(calculator.sub(-.5), 19.61)
-        self.assertEqual(calculator.sub(-5e3), 5019.61)
-        self.assertEqual(calculator.sub(5e3), 19.61)
-        self.assertEqual(calculator.sub(0), 19.61)
-        self.assertEqual(calculator.sub(-0), 19.61)
+        for key, result in zippedkeys:
+            self.assertEqual(calculator.sub(key), result)
 
     def test_mul_1(self):
+        results = [0] * len(validkeys)
+        zippedkeys = zip(validkeys, results)
         calculator = MathOperations()
         self.assertEqual(calculator.getvalue(), 0)
-        self.assertEqual(calculator.mul(10), 0)
-        self.assertEqual(calculator.mul(-20), 0)
-        self.assertEqual(calculator.mul(9.78), 0)
-        self.assertEqual(calculator.mul(-19.39), 0)
-        self.assertEqual(calculator.mul(.5), 0)
-        self.assertEqual(calculator.mul(-.5), 0)
-        self.assertEqual(calculator.mul(-5e3), 0)
-        self.assertEqual(calculator.mul(5e3), 0)
-        self.assertEqual(calculator.mul(0), 0)
-        self.assertEqual(calculator.mul(-0), 0)
+        for key, result in zippedkeys:
+            self.assertEqual(calculator.mul(key), result)
 
     def test_mul_2(self):
+        results = [1] * len(validkeys)
+        for i, _ in enumerate(validkeys):
+            if i == 0:
+                results[i] *= validkeys[i]
+            else:
+                results[i] = results[i-1] * validkeys[i]
+        zippedkeys = zip(validkeys, results)
         calculator = MathOperations(1)
         self.assertEqual(calculator.getvalue(), 1)
-        self.assertEqual(calculator.mul(10), 10)
-        self.assertEqual(calculator.mul(-20), -200)
-        self.assertEqual(calculator.mul(9.78), -1956)
-        self.assertEqual(calculator.mul(-19.5), 38142)
-        self.assertEqual(calculator.mul(.5), 19071)
-        self.assertEqual(calculator.mul(-.5), -9535.5)
-        self.assertEqual(calculator.mul(-1e-3), 9.5355)
-        self.assertEqual(calculator.mul(1e4), 95355)
-        self.assertEqual(calculator.mul(0), 0)
-        self.assertEqual(calculator.mul(-0), 0)
+        for key, result in zippedkeys:
+            self.assertEqual(calculator.mul(key), result)
+
+    def test_mul_3(self):
+        results = [1] * 4
+        restofkeys = validkeys[-4:]
+        for i, _ in enumerate(restofkeys):
+            if i == 0:
+                results[i] *= restofkeys[i]
+            else:
+                results[i] = results[i-1] * restofkeys[i]
+        zippedkeys = zip(validkeys, results)
+        calculator = MathOperations(1)
+        self.assertEqual(calculator.getvalue(), 1)
+        for key, result in zippedkeys:
+            self.assertEqual(calculator.mul(key), result)
 
     def test_div_1(self):
+        results = [0] * len(validkeys)
+        zippedkeys = zip(validkeys, results)
         calculator = MathOperations()
         self.assertEqual(calculator.getvalue(), 0)
-        self.assertEqual(calculator.div(10), 0)
-        self.assertEqual(calculator.div(-20), 0)
-        self.assertEqual(calculator.div(9.78), 0)
-        self.assertEqual(calculator.div(-19.39), 0)
-        self.assertEqual(calculator.div(.5), 0)
-        self.assertEqual(calculator.div(-.5), 0)
-        self.assertEqual(calculator.div(-5e3), 0)
-        self.assertEqual(calculator.div(5e3), 0)
+        for key, result in zippedkeys:
+            if key == 0:
+                self.assertRaises(calculator.div(key), ZeroDivisionError)
+            else:
+                self.assertEqual(calculator.div(key), result)
+
+    def test_div_2(self):
+        results = [1] * len(validkeysdiv)
+        for i, _ in enumerate(validkeysdiv):
+            if i == 0:
+                results[i] /= validkeysdiv[i]
+            else:
+                results[i] = results[i-1] / validkeysdiv[i]
+        zippedkeys = zip(validkeysdiv, results)
+        calculator = MathOperations(1)
+        self.assertEqual(calculator.getvalue(), 1)
+        for key, result in zippedkeys:
+            if key == 0:
+                self.assertRaises(calculator.div(key), ZeroDivisionError)
+            else:
+                self.assertEqual(calculator.div(key), result)
         self.assertRaises(calculator.div(0), ZeroDivisionError)
         self.assertRaises(calculator.div(-0), ZeroDivisionError)
 
@@ -126,32 +165,47 @@ class TestBasicMathOperations(unittest.TestCase):
         self.assertRaises(calculator.factorial(), ValueError)
 
     def test_pow_1(self):
+        results = [0] * len(validkeys)
+        zippedkeys = zip(validkeys, results)
         calculator = MathOperations()
         self.assertEqual(calculator.getvalue(), 0)
-        self.assertEqual(calculator.pow(10), 0)
-        self.assertEqual(calculator.pow(-20), 0)
-        self.assertEqual(calculator.pow(9.78), 0)
-        self.assertEqual(calculator.pow(-19.39), 0)
-        self.assertEqual(calculator.pow(.5), 0)
-        self.assertEqual(calculator.pow(-.5), 0)
-        self.assertEqual(calculator.pow(-5e3), 0)
-        self.assertEqual(calculator.pow(5e3), 0)
-        self.assertEqual(calculator.pow(0), 0)
-        self.assertEqual(calculator.pow(-0), 0)
+        for key, result in zippedkeys:
+            self.assertEqual(calculator.pow(key), result)
+
+    def test_pow_2(self):
+        results = [2] * len(validkeys)
+        for i, _ in enumerate(validkeys):
+            if i == 0:
+                results[i] = results[i] ** validkeys[i]
+            else:
+                results[i] = results[i-1] ** validkeys[i]
+        zippedkeys = zip(validkeys, results)
+        calculator = MathOperations(2)
+        self.assertEqual(calculator.getvalue(), 2)
+        for key, result in zippedkeys:
+            self.assertEqual(calculator.pow(key), result)
 
     def test_sqrt_1(self):
+        results = [0] * len(validkeys)
+        zippedkeys = zip(validkeys, results)
         calculator = MathOperations()
         self.assertEqual(calculator.getvalue(), 0)
-        self.assertEqual(calculator.sqrt(10), 0)
-        self.assertEqual(calculator.sqrt(-20), 0)
-        self.assertEqual(calculator.sqrt(9.78), 0)
-        self.assertEqual(calculator.sqrt(-19.39), 0)
-        self.assertEqual(calculator.sqrt(.5), 0)
-        self.assertEqual(calculator.sqrt(-.5), 0)
-        self.assertEqual(calculator.sqrt(-5e3), 0)
-        self.assertEqual(calculator.sqrt(5e3), 0)
-        self.assertEqual(calculator.sqrt(0), 0)
-        self.assertEqual(calculator.sqrt(-0), 0)
+        for key, result in zippedkeys:
+            self.assertEqual(calculator.sqrt(key), result)
+
+    def test_sqrt_2(self):
+        results = [2] * len(validkeysdiv)
+        for i, _ in enumerate(validkeysdiv):
+            if i == 0:
+                results[i] = results[i] ** (1 / validkeysdiv[i])
+            else:
+                results[i] = results[i-1] ** (1 / validkeysdiv[i])
+        zippedkeys = zip(validkeysdiv, results)
+        calculator = MathOperations(2)
+        self.assertEqual(calculator.getvalue(), 2)
+        for key, result in zippedkeys:
+            self.assertEqual(calculator.sqrt(key), result)
+        self.assertRaises(calculator.sqrt(0), ZeroDivisionError)
 
 
 class TestAdvancedMathOperations(unittest.TestCase):
