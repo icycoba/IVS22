8bd2e7d7caa50aeba9e47e5b834211bac54169cf
xkundr07 <64934497+obza123@users.noreply.github.com>
2022-04-15 21:38:04 +0200

    skoro, ale faktorial a sin jeste ne
    tak nejak skoro uz bych to i pouzil jako kalkulacku
    ale faktorial a sinus mi nejdriv vyhazoval divne hodnoty, tak jsem +-dokoncil zbytek a ty 2 jeste nemam.
    taky se může ten text co se zobrazí uživatelem vymazat tak to možná ještě změnit, + input zatím jen klikáním na tlačítka, psaní textu nic neudělá.

--- src/guitest.py
+++ src/guitest.py
@@ -6,13 +6,16 @@ from tkinter import messagebox
 # okno
 root = tkinter.Tk()
 root['bg'] = '#FAFAFA'
-root.geometry('380x600')
+root.geometry('405x600')
 root.resizable(False, False)
 root.title('Kalkulačka')
 
 
 ix = 0
 iy = 0
+iz = 0
+
+stridacka = 0
 
 vysledek = 0
 text = Text(root, height='4')
@@ -21,26 +24,35 @@ text = Text(root, height='4')
 qwe = PanedWindow(bg='#ECECEC')
 asd = PanedWindow(bg='#ECECEC')
 yxc = PanedWindow(bg='#ECECEC')
+uio = PanedWindow(bg='#ECECEC')
 
 # proměnné pro výpočty
 sety = 0
 setx = 0
+setz = 0
 bignum = 1
 num_end = False
 op = 0
-lastop = 0
-opcodes = ["=", "+", "-"]
+lastop = [0, 0]
+opcodes = ["=", "+", "-", "×", "÷", "^", "^1/", "!", "sin "]
 
 # načtení čísla
 def test(num):
     global ix
     global iy
+    global iz
     global setx
     global sety
+    global setz
     global bignum
-    if setx == 1 and sety == 1:
+    global stridacka
+    if setx == 1 and sety == 1 and setz == 1:
+        ix = 0
         setx = 0
         sety = 0
+        setz = 0
+        text.delete('1.0', END)
+        
     if setx == 0 and sety == 0:
         ix = ix*bignum+num
         bignum = 10
@@ -49,55 +61,188 @@ def test(num):
         iy = iy*bignum+num
         bignum=10
         
-    text.delete('1.0', END)
-    #text.insert(END,str(ix)+"("+str(setx)+", "+str(sety)+") ")
-    text.insert(END, str(ix)+opcodes[lastop]+str(iy))
+    elif setx == 1 and sety == 1:
+        iz = iz*bignum+num
+        bignum=10
 
+
+    text.insert(END, num)
+    
+    # dobrý výpis pro hledání chyb
+    #text.delete('1.0', END)
+    #text.insert(END, str(ix)+opcodes[lastop[0]]+str(iy)+opcodes[lastop[1]]+str(iz))
+    stridacka = 1
+    
 # načtení a provedení operace
 def operace(op):
-    global ix
-    global iy
-    global setx
-    global sety
-    global bignum
-    global operations
-    global vysledek
-    global lastop
-    operations = ["", mathlib.MathOperations(ix).add, mathlib.MathOperations(ix).sub]
-    bignum = 1
-    if setx == 0 and sety == 0 and op != 0:
+    
+    global stridacka
+    if stridacka == 1:
+        global ix
+        global iy
+        global iz
+        global setx
+        global sety
+        global setz
+        global bignum
+        global operations
+        global vysledek
+        global lastop
+        
+        
+        operations_x = ["",
+                      mathlib.MathOperations(ix).add,       mathlib.MathOperations(ix).sub,
+                      mathlib.MathOperations(ix).mul,       mathlib.MathOperations(ix).div,
+                      mathlib.MathOperations(ix).pow,       mathlib.MathOperations(ix).root,
+                      mathlib.MathOperations(ix).factorial, mathlib.MathOperations(ix).sin
+                      
+                      ]
+        
+        operations_y = ["",
+                      mathlib.MathOperations(iy).add,       mathlib.MathOperations(iy).sub,
+                      mathlib.MathOperations(iy).mul,       mathlib.MathOperations(iy).div,
+                      mathlib.MathOperations(iy).pow,       mathlib.MathOperations(iy).root,
+                      mathlib.MathOperations(iy).factorial, mathlib.MathOperations(iy).sin
+                      
+                      ]
+        
+        
+        text.insert(END, opcodes[op])
+        bignum = 1
+        if setx == 1 and sety == 1 and setz == 1:
+            setx = 0
+            sety = 0
+            setz = 0
+        
+        
+        if setx == 0 and sety == 0 and op != 0:
+            setx = 1
+            lastop[0] = op
+            #bignum = 1
+        elif setx == 1 and sety == 0 and lastop[0] >= 3 and lastop[0] <= 6 and op != 0:
+            ix = operations_x[lastop[0]](iy)
+            iy = 0
+            lastop[0] = op
+        elif setx == 1 and sety == 0 and op == 1 or op == 2:
+            ix = operations_x[lastop[0]](iy)
+            iy = 0
+            lastop[0] = op
+        elif setx == 1 and sety == 0 and op >= 3 and op <= 6:
+            sety = 1
+            lastop[1] = op
+        elif setx == 1 and sety == 1 and op != 0:
+            if lastop[0] < 3 and lastop[1] < 3:
+                ix = operations_x[lastop[0]](iy)
+                iy = iz
+                lastop[0] = lastop[1]
+            else:
+                iy = operations_y[lastop[1]](iz)
+            lastop[1] = op
+            iz = 0
+        elif setx == 1 and sety == 1 and setz == 0 and op == 0:
+            iy = operations_y[lastop[1]](iz)
+            vysledek = operations_x[lastop[0]](iy)
+            ix = vysledek
+            iy = 0
+            iz = 0
+            setz = 1
+            text.delete('1.0', END)
+            text.insert(END,vysledek)
+        elif setx == 1 and sety == 0 and op == 0:
+            try:
+                vysledek = operations_x[lastop[0]](iy)
+                text.delete('1.0', END)
+                text.insert(END,vysledek)
+            except (TypeError, ZeroDivisionError, ValueError):
+                text.delete('1.0', END)
+                text.insert(END,"Chyba v zadání výpočtu!")
+                pass
+            ix = vysledek
+            iy = 0
+            iz = 0
+            sety = 1
+            setz = 1
+        elif op == 0:
+            if ix == 0:
+                ix = vysledek
+            text.delete('1.0', END)
+            text.insert(END,str(ix))
+            setx = 1
+            sety = 1
+            setz = 1
+            #ix = 0
+    if op == 0:
+        stridacka = 1
+    else:
+        stridacka = 0
+        
+        #puvodni + mala uprava, smazat asi pak.
+"""    if setx == 0 and sety == 0 and op != 0:
         setx = 1
         lastop = op
-    elif setx == 1 and sety == 0 and op != 0:
+        bignum = 1
+        
+    elif setx == 1 and sety == 0 and op != 0 and op < 7:
         ix = operations[lastop](iy)
+        lastop = op
+        iy = 0
+    elif setx == 1 and sety == 0 and op > 6:
+        ix = operations[lastop]
         lastop = op
         iy = 0
     elif setx == 1 and sety == 0 and op == 0:
         sety = 1
+        bignum = 1
         text.delete('1.0', END)
-        vysledek = operations[lastop](iy)
+        if lastop < 7:
+            vysledek = operations[lastop](iy)
+        else:
+            vysledek = operations[lastop]
         ix = 0
         iy = 0
-        text.insert(END,str(vysledek))
-    
+        text.insert(END,vysledek)
+    elif op == 0:
+        text.delete('1.0', END)
+        text.insert(END,str(ix))
+        ix = 0"""
     
 # přiřazení funkcí tlačítkam
-q = Button(qwe, text="1", width='12',height='4', borderwidth='1', command=lambda: test(1))  #1 cislo = 8 px 3 buttony : 3*8 = 24, celkem/24 = 100/24=4 
-w = Button(qwe, text="2", width='12',height='4', borderwidth='1', command=lambda: test(2))
-e = Button(qwe, text="3", width='12',height='4', borderwidth='1', command=lambda: test(3))
-r = Button(qwe, text="+", width='12',height='4', borderwidth='1', command=lambda: operace(1))
+q = Button(qwe, text="1", width='10',height='4', borderwidth='1', command=lambda: test(1))  #1 cislo = 8 px 3 buttony : 3*8 = 24, celkem/24 = 100/24=4 
+w = Button(qwe, text="2", width='10',height='4', borderwidth='1', command=lambda: test(2))
+e = Button(qwe, text="3", width='10',height='4', borderwidth='1', command=lambda: test(3))
+r = Button(qwe, text="+", width='10',height='4', borderwidth='1', command=lambda: operace(1))
+t = Button(qwe, text="-", width='10',height='4', borderwidth='1', command=lambda: operace(2))
 
-a = Button(asd, text="4", width='12',height='4', borderwidth='1', command=lambda: test(4))
-s = Button(asd, text="5", width='12',height='4', borderwidth='1', command=lambda: test(5))
-d = Button(asd, text="6", width='12',height='4', borderwidth='1', command=lambda: test(6))
-f = Button(asd, text="-", width='12',height='4', borderwidth='1', command=lambda: operace(2))
+a = Button(asd, text="4", width='10',height='4', borderwidth='1', command=lambda: test(4))
+s = Button(asd, text="5", width='10',height='4', borderwidth='1', command=lambda: test(5))
+d = Button(asd, text="6", width='10',height='4', borderwidth='1', command=lambda: test(6))
+f = Button(asd, text="×", width='10',height='4', borderwidth='1', command=lambda: operace(3))
+g = Button(asd, text="÷", width='10',height='4', borderwidth='1', command=lambda: operace(4))
 
-y = Button(yxc, text="7", width='12',height='4', borderwidth='1', command=lambda: test(7))
-x = Button(yxc, text="8", width='12',height='4', borderwidth='1', command=lambda: test(8))
-c = Button(yxc, text="9", width='12',height='4', borderwidth='1', command=lambda: test(9))
-v = Button(yxc, text="=", width='12',height='4', borderwidth='1', command=lambda: operace(0))
+
+y = Button(yxc, text="7", width='10',height='4', borderwidth='1', command=lambda: test(7))
+x = Button(yxc, text="8", width='10',height='4', borderwidth='1', command=lambda: test(8))
+c = Button(yxc, text="9", width='10',height='4', borderwidth='1', command=lambda: test(9))
+v = Button(yxc, text="a^n", width='10',height='4', borderwidth='1', command=lambda: operace(5))
+b = Button(yxc, text="n√a", width='10',height='4', borderwidth='1', command=lambda: operace(6))
+
+
+
+# faktorial a sinus nefunguji, nevim proc, hazi to tam:
+#<bound method MathOperations.factorial of <mathlib.MathOperations object at 0x000001C6BFC2D640>>
+# tak nevim co s tim moc.
+# taky jeste nema uplne deleni/nasobeni prednost.
+u = Button(uio, text="0", width='10',height='4', borderwidth='1', command=lambda: test(0))
+i = Button(uio, text="a!", width='10',height='4', borderwidth='1', command=lambda: operace(7))
+o = Button(uio, text="sin a", width='10',height='4', borderwidth='1', command=lambda: operace(8))
+p = Button(uio, text="=", width='20',height='4', borderwidth='1', command=lambda: operace(0))
 
 # pruhy s tlačítky na spodek
+uio.pack(
+    side=BOTTOM,
+    fill=X
+    )
+
 yxc.pack(
     side=BOTTOM,
     fill=X
@@ -121,16 +266,24 @@ qwe.add(q)
 qwe.add(w)
 qwe.add(e)
 qwe.add(r)
+qwe.add(t)
 
 asd.add(a)
 asd.add(s)
 asd.add(d)
 asd.add(f)
+asd.add(g)
 
 yxc.add(y)
 yxc.add(x)
 yxc.add(c)
 yxc.add(v)
+yxc.add(b)
+
+uio.add(u)
+uio.add(i)
+uio.add(o)
+uio.add(p)
 
 
 root.mainloop()
