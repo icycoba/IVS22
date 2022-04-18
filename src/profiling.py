e1a9e875e6f4a776f259ca5bc35cd5d5aca1a3ed
icycoba <icycoba@gmail.com>
2022-04-18 15:36:43 +0200

    Add mean() to profiling.

--- src/profiling.py
+++ src/profiling.py
@@ -1,13 +1,32 @@
 """
 Soubor:     profiling.py
 Autor:      Petra Dudová    (xdudov02)
+            Martin Hlinský  (xhlins01)
 Projekt:    IVS #2 - Tvorba kalkulačky
 Popis:      Program pro výpočet výběrové směrodatné odchylky
 """
 
-from mathlib import MathOperations
+from expression_to_num import MathSolver
+import sys
+
+
+def mean(numarray):
+    result = 0
+    for num in numarray:
+        result += num
+    result /= len(numarray)
+    return result
+
 
 if __name__ == "__main__":
-    MathOperations.sub(2, 2)
+    # Ošetřit správné otevření souboru
+    numarray = sys.stdin.read()
+    numarray = numarray.split()
+    try:
+        numarray = list(map(int, numarray))
+    except ValueError as e:
+        print(f"ValueError: Jedna z hodnot nelze převést na číslo.")
+
+    print(numarray)
 
 # ...
