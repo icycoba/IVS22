43629900bcc0a5cf6a3d4e0699f4a786415a8fdc
icycoba <icycoba@gmail.com>
2022-04-19 02:34:49 +0200

    Progress on profiling, deviationsum finished; still needs some work.

--- src/profiling.py
+++ src/profiling.py
@@ -6,16 +6,38 @@ Projekt:    IVS #2 - Tvorba kalkulačky
 Popis:      Program pro výpočet výběrové směrodatné odchylky
 """
 
-from expression_to_num import MathSolver
+from expression_to_num import MathSolver, MathOperations
+# from mathlib import MathOperations
 import sys
 
 
-def mean(numarray):
-    result = 0
-    for num in numarray:
-        result += num
-    result /= len(numarray)
+def mean(narray):
+    # xmean = 1/N * sum(x_i)
+    result = MathOperations()
+    for num in narray:
+        result.add(num)
+    result.div(len(narray))
+    return result.getvalue()
+
+
+def deviationsum(narray, xmean):
+    result = MathOperations()
+    nxmeansquared = float(MathSolver().solve(f"{len(narray)}*{xmean}^2"))
+    arraylen = float(MathSolver().solve(f"{len(narray)}-1"))
+    # print(nxmeansquared)
+    for num in narray:
+        tempnum = MathOperations(num)
+        tempnum.pow(2)
+        result.add(tempnum.getvalue())
+    result.sub(nxmeansquared)
+    result.div(arraylen)
+    result.root(2)
     return result
+
+
+def deviation(narray, xmean):
+    # sqrt(1/N-1 * (sum(x_i^2 - N * x_mean^2)))
+    pass
 
 
 if __name__ == "__main__":
@@ -26,7 +48,10 @@ if __name__ == "__main__":
         numarray = list(map(int, numarray))
     except ValueError as e:
         print(f"ValueError: Jedna z hodnot nelze převést na číslo.")
+        exit(1)
 
-    print(numarray)
+    x = mean(numarray)
+    result = deviationsum(numarray, x)
+    print(result.getvalue())
 
 # ...
